af6dbe55265656fff5c48b1d5f26ab4e4088157e
Claude Ayitey <clauditey@gmail.com>
2019-10-09 12:28:25 +0000

    GA refactor

--- index.js
+++ index.js
@@ -1,10 +1,8 @@
-<script async src = "https://www.googletagmanager.com/gtag/js?id=UA-46316096-4" > < /script> <
-    script >
-    window.dataLayer = window.dataLayer || [];
+window.dataLayer = window.dataLayer || [];
 
 function gtag() {
     dataLayer.push(arguments);
 }
 gtag('js', new Date());
 
-gtag('config', 'UA-46316096-4'); </script>
+gtag('config', 'UA-46316096-4');
