5d693c9926f509995f5340e96b6d468b3d508837
Claude Ayitey <clauditey@gmail.com>
2019-10-09 15:18:45 +0000

    removed axios callback. It was increased form submissions unnecessarily.

--- index.js
+++ index.js
@@ -7,14 +7,3 @@ gtag('js', new Date());
 
 gtag('config', 'UA-46316096-4');
 
-
-axios
-    .post("https://submit-form.com/jay6HuL4YjXhI3yWQEqny", {
-        message: "Thanks for your interest."
-    })
-    .then(function (response) {
-        console.log(response);
-    })
-    .catch(function (response) {
-        console.error(response);
-    });
