52f42cd8117a68c634eaeebd16906a03ae5d446f
Claude Ayitey <clauditey@gmail.com>
2019-10-09 11:04:15 +0000

    added Google Analytics code.

---
+++ index.js
@@ -0,0 +1,10 @@
+<script async src = "https://www.googletagmanager.com/gtag/js?id=UA-46316096-4" > < /script> <
+    script >
+    window.dataLayer = window.dataLayer || [];
+
+function gtag() {
+    dataLayer.push(arguments);
+}
+gtag('js', new Date());
+
+gtag('config', 'UA-46316096-4'); </script>
