082d457576c2bcf1c160022068abaa7c984135c9
Claude Ayitey <clauditey@gmail.com>
2019-10-09 13:35:31 +0000

    trying AJAX form send with axios.

--- index.js
+++ index.js
@@ -6,3 +6,15 @@ function gtag() {
 gtag('js', new Date());
 
 gtag('config', 'UA-46316096-4');
+
+
+axios
+    .post("https://submit-form.com/jay6HuL4YjXhI3yWQEqny", {
+        message: "Thanks for your interest."
+    })
+    .then(function (response) {
+        console.log(response);
+    })
+    .catch(function (response) {
+        console.error(response);
+    });
